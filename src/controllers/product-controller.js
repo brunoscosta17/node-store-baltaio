455f45e651fb38dc4e850dcd86023db0a2d598ea
= <=>
2019-04-08 21:44:01 -0300

    Aula 17 - Listando os produtos

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,14 +3,27 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = (req, res, next) => {
+    Product.find({active: true}, 'title price slug')
+        .then( data => {
+            res.status(200).send({data});
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Erro ao buscar o produto!',
+                data: e
+            });
+        });
+};
+
 exports.post = (req, res, next) => {
     var product = new Product(req.body);
     product.save()
-        .then( x => {
-            res.status(201).send({message: 'Produto cadastrado com sucesso!'});
+        .then( data => {
+            res.status(201)
+                .send({data});
         }).catch(e => {
             res.status(400).send({
-                message: 'Erro ao cadastrar o produto!',
+                message: 'Erro ao buscar produto!',
                 data: e
             });
         });
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -1,8 +1,9 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/ptoduct-controller');
+const controller = require('../controllers/product-controller');
 
+router.get('/', controller.get);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
