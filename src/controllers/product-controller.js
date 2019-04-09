e6159c0748ab890ec201c0428c1eeb3d5de12cc3
= <=>
2019-04-08 22:23:24 -0300

    Aula 18 - Listando um produto pelo slug

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -4,12 +4,31 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 exports.get = (req, res, next) => {
-    Product.find({active: true}, 'title price slug')
+    Product
+        .find({
+            active: true
+        }, 'title price slug')
         .then( data => {
             res.status(200).send({data});
         }).catch(e => {
             res.status(400).send({
                 message: 'Erro ao buscar o produto!',
+                data: e
+            });
+        });
+};
+
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            slug: req.params.slug,
+            active: true
+        }, 'title description price slug tags')
+        .then( data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Erro ao buscar slug!',
                 data: e
             });
         });
