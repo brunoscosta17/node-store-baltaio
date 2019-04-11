548102e96616df7c2c5a0f34e7ddf41b9ec65509
= <=>
2019-04-10 22:28:24 -0300

    Aula 24 - Repositórios

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,14 +3,13 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 const ValidatorContract = require('../validators/fluent-validator');
+const repository = require('../repositories/product-repository');
 
 exports.get = (req, res, next) => {
-    Product
-        .find({
-            active: true
-        }, 'title price slug')
+    repository
+        .get()
         .then(data => {
-            res.status(200).send({ data });
+            res.status(200).send( data );
         }).catch(e => {
             res.status(400).send({
                 message: 'Erro ao buscar o produto!',
@@ -20,11 +19,8 @@ exports.get = (req, res, next) => {
 };
 
 exports.getBySlug = (req, res, next) => {
-    Product
-        .findOne({
-            slug: req.params.slug,
-            active: true
-        }, 'title description price slug tags')
+    repository
+        .getBySlug(req.params.slug)
         .then(data => {
             res.status(200).send(data);
         }).catch(e => {
@@ -36,8 +32,8 @@ exports.getBySlug = (req, res, next) => {
 };
 
 exports.getById = (req, res, next) => {
-    Product
-        .findById(req.params.id)
+    repository
+        .getById(req.params.id)
         .then(data => {
             res.status(200).send(data);
         }).catch(e => {
@@ -49,11 +45,8 @@ exports.getById = (req, res, next) => {
 };
 
 exports.getByTag = (req, res, next) => {
-    Product
-        .find({
-            tags: req.params.tag,
-            active: true
-        }, 'title description price slug tags')
+    repository
+        .getByTag(req.params.tag)
         .then(data => {
             res.status(200).send({ data });
         }).catch(e => {
@@ -76,8 +69,7 @@ exports.post = (req, res, next) => {
         return;
     };
 
-    var product = new Product(req.body);
-    product.save()
+    repository.create(req.body)
         .then(data => {
             res.status(201)
                 .send({ data });
@@ -90,14 +82,9 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    Product
-        .findByIdAndUpdate(req.params.id, {
-            $set: {
-                title: req.body.title,
-                description: req.body.description,
-                price: req.body.price
-            }
-        }).then(data => {
+    repository
+        .update(req.params.id, req.body)
+        .then(data => {
             res.status(200).send({
                 message: 'Produto atualizado com sucesso!'
             });
@@ -110,8 +97,8 @@ exports.put = (req, res, next) => {
 };
 
 exports.delete = (req, res, next) => {
-    Product
-        .findOneAndRemove(req.body.id)
+    repository
+        .delete(req.body.id)
         .then(data => {
             res.status(200).send({
                 message: 'Produto removido com sucesso!',
