9aef76ef4fc0c3c357602250f34cc67a5e1f4a92
= <=>
2019-04-13 13:59:41 -0300

    Aula 25 - Async/Await

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -5,59 +5,55 @@ const Product = mongoose.model('Product');
 const ValidatorContract = require('../validators/fluent-validator');
 const repository = require('../repositories/product-repository');
 
-exports.get = (req, res, next) => {
-    repository
-        .get()
-        .then(data => {
-            res.status(200).send( data );
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Erro ao buscar o produto!',
-                data: e
-            });
+exports.get = async (req, res, next) => {
+    try {
+        var data = await repository.get();
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição!',
+            data: error
         });
+    }
 };
 
-exports.getBySlug = (req, res, next) => {
-    repository
-        .getBySlug(req.params.slug)
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Erro ao buscar slug!',
-                data: e
-            });
+exports.getBySlug = async (req, res, next) => {
+    try {
+        var data = await repository.getBySlug(req.params.slug);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição!',
+            data: error
         });
+    };
 };
 
-exports.getById = (req, res, next) => {
-    repository
-        .getById(req.params.id)
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Erro ao buscar slug!',
-                data: e
-            });
+exports.getById = async (req, res, next) => {
+    try {
+        var data = await repository.getById(req.params.id);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição!',
+            data: error
         });
+    };
 };
 
-exports.getByTag = (req, res, next) => {
-    repository
-        .getByTag(req.params.tag)
-        .then(data => {
-            res.status(200).send({ data });
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Erro ao buscar o produto!',
-                data: e
-            });
+exports.getByTag = async (req, res, next) => {
+    try {
+        var data = await repository.getByTag(req.params.tag);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição!',
+            data: error
         });
+    };
 };
 
-exports.post = (req, res, next) => {
+exports.post = async (req, res, next) => {
     let contract = new ValidatorContract();
     contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres!');
     contract.hasMinLen(req.body.slug, 3, 'O slug deve conter pelo menos 3 caracteres!');
@@ -69,45 +65,42 @@ exports.post = (req, res, next) => {
         return;
     };
 
-    repository.create(req.body)
-        .then(data => {
-            res.status(201)
-                .send({ data });
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Erro ao buscar produto!',
-                data: e
-            });
+    try {
+        await repository.create(req.body);
+        res.status(201).send({
+            message: 'Produto cadastrado com sucesso!' });
+    } catch (error) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição!',
+            data: error
         });
+    };
+
 };
 
-exports.put = (req, res, next) => {
-    repository
-        .update(req.params.id, req.body)
-        .then(data => {
-            res.status(200).send({
-                message: 'Produto atualizado com sucesso!'
-            });
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Falha ao atualizar o produto!',
-                data: e
-            });
-        })
+exports.put = async (req, res, next) => {
+    try {
+        await repository.update(req.params.id, req.body);
+        res.status(200).send({
+            message: 'Produto atualizado com sucesso!'
+        });     
+    } catch (error) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição!',
+            data: error
+        });
+    }
 };
 
-exports.delete = (req, res, next) => {
-    repository
-        .delete(req.body.id)
-        .then(data => {
-            res.status(200).send({
-                message: 'Produto removido com sucesso!',
-                data: data
-            });
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Falha ao remover o produto!',
-                data: e
-            });
-        })
+exports.delete = async (req, res, next) => {
+    try {
+        await repository.delete(req.body.id)
+        res.status(200).send({
+            message: 'Produto removido com sucesso!'
+        });        
+    } catch (error) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição!'
+        });
+    }
 };
