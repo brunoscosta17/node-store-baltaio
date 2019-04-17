1033537b616ff2b9d0a571d57620e8cd35f484c7
= <=>
2019-04-17 11:39:19 -0300

    Aula 29 - Revisitando os Controllers - Order

--- src/app.js
+++ src/app.js
@@ -13,12 +13,13 @@ mongoose.connect('mongodb://bruno:mongo471577@ds133556.mlab.com:33556/ndstr_2');
 //Carrega os Models
 const Product = require('./models/product-model');
 const Customer = require('./models/customer-model');
-const Order = require('./models/customer-order')
+const Order = require('./models/order-model')
 
 //Carrega as Rotas
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
 const customerRoute = require('./routes/customer-route');
+const orderRoute = require('./routes/order-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,5 +27,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', indexRoute);
 app.use('/products', productRoute);
 app.use('/customers', customerRoute);
+app.use('/orders', orderRoute);
 
 module.exports = app;
