02ddc8f1c1311930611f9e42420c03957a299513
= <=>
2019-04-22 02:14:10 -0300

    Aula 32 - Enviando E-mail de boas vindas

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb://bruno:mongo471577@ds133556.mlab.com:33556/ndstr_2',
-    sendgridKey: 'SUA CHAVE',
+    sendgridKey: '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',
     containerConnectionString: 'SUA CONNECTION STRING'
 }
