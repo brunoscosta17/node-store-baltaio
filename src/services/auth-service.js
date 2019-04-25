218cac2ceca6f890f938f19f6b92097e78692e69
= <=>
2019-04-25 09:53:33 -0300

    Aula 37 - Autorização

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -12,7 +12,6 @@ exports.decodeToken = async (token) => {
 };
 
 exports.authorize = function (req, res, next) {
-    var token = req.body.token || req.query.token
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
     if (!token) {
@@ -29,5 +28,31 @@ exports.authorize = function (req, res, next) {
                 next();
             }
         });
+    }
+};
+
+exports.isAdmin = function (req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    if (!token) {
+        res.status(401).json({
+            message: 'Token inválido!'
+        });
+    } else {
+        jwt.verify(token, global.SALT_KEY, function (error, decoded) {
+            if (error) {
+                res.status(401).json({
+                    message: 'Token inválido!'
+                })
+            } else {
+                if (decoded.roles.includes('admin')) { // se a string 'admin' está dentro do array 'roles'
+                    next();
+                } else {
+                    res.status(403).json({
+                        message: 'Esta funcionalidade é restrita para administradores!'
+                    });
+                }
+            }
+        })
     }
 };
