4807bf45c41ed942303b7cdba873c97533298597
= <=>
2019-04-25 21:39:31 -0300

    Aula 38 - Outros

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-acces-token');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
 
 app.use('/', indexRoute);
